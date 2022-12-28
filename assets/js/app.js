938431a20ba32e1c55667ecd9bdffc57b1fdaa20
OzgunEfe <ozgunefe.dev@gmail.com>
2022-12-28 22:12:05 +0000

    I created some localStorage functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,21 @@
-var apiKey = '869340399bd2cd397673477060de7d0b';
-var city = 'London';
+var apiKey = "869340399bd2cd397673477060de7d0b";
+var city = "London";
 var baseUrl = "https://api.openweathermap.org/data/2.5/";
 var currentUrl = baseUrl + `weather?appid=${apiKey}&units=metric&`;
 var forecastUrl = baseUrl + `forecast?appid=${apiKey}&units=metric&`;
-var iconUrl = 'https://openweathermap.org/img/w/';
+var iconUrl = "https://openweathermap.org/img/w/";
 var currentDate = moment().format("DD/MM/YYYY");
 
-console.log(currentDate);
 
-forecastInfoArray = JSON.parse(localStorage.getItem("forecast")) || [];
+cityNamesArray = JSON.parse(localStorage.getItem("forecast")) || [];
 
+// This function gets the forecast data and shows it on the screen.
 function inputSubmitted(cityName) {
-    $.get(currentUrl + `q=${cityName}`)
-        .then(function(currentData) {
-            $('.daily-forecast').removeClass("hide");
-            $('.daily-forecast').append(`
+  if (cityNamesArray.includes(cityName)) {
+  } else {
+    $.get(currentUrl + `q=${cityName}`).then(function (currentData) {
+      $(".daily-forecast").removeClass("hide");
+      $(".daily-forecast").append(`
                 <div class="forecast-header-section">
                     <h2>${cityName} (${currentDate})</h2>
                     <img src="${iconUrl + currentData.weather[0].icon}.png" alt="weather-icon" width="45px" height="45px">
@@ -22,83 +23,83 @@ function inputSubmitted(cityName) {
                     <p>Temp: ${Math.round(currentData.main.temp)}°C</p>
                     <p>Wind: ${currentData.wind.speed} KPH</p>
                     <p id="daily-forecast-last-item">Humidity: ${currentData.main.humidity}%</p>
-            `);
+        `);
 
-            curentForecast = {
-                CityName: cityName,
-                CurrentDate: currentDate,
-                Temp: Math.round(currentData.main.temp),
-                Wind: currentData.wind.speed,
-                Humidity: currentData.main.humidity
-            }
+      $(".fiveDayForevastTitle").removeClass("hide");
 
-            forecastInfoArray.push(curentForecast);
+      $.get(forecastUrl +`lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`)
+        .then(function (forecastData) {
+            for (var castObj of forecastData.list) {
+                var date = moment(castObj.dt_txt.split(" ")[0]).format("DD/MM/YYYY");
 
-            $('.fiveDayForevastTitle').removeClass("hide");
+                if (castObj.dt_txt.split(" ")[1] === "09:00:00") {
+                    $(".fiveDays-forecast").append(`
+                        <div class="forecast-boxes">
+                            <h4>${date}</h4>
+                            <img src="${
+                              iconUrl + castObj.weather[0].icon
+                            }.png" width="35px" height="35px" alt="weather-icon">
+                            <p>Temp: ${Math.round(castObj.main.temp)}°C</p>
+                            <p>Wind: ${castObj.wind.speed} KPH</p>
+                            <p>Humidity: ${castObj.main.humidity}%</p>
+                        </div>
+                    `);
+                };
+            };
 
-            $.get(forecastUrl + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`)
-                .then(function(forecastData) {
-                    for (var castObj of forecastData.list) {
+        if (forecastData.cod == "200") {
+          if (cityNamesArray.includes(cityName)) {
+          } else {
+                $(".historyCityName").removeClass("hide");
+                $(".historyCityName").append(`
+                    <button class="btn cityNameButtons">${cityName}</button>
+                `);
 
-                        var date = moment(castObj.dt_txt.split(' ')[0]).format("DD/MM/YYYY");
-
-                        if(castObj.dt_txt.split(' ')[1] === "09:00:00") {
-                            $('.fiveDays-forecast').append(`
-                            <div class="forecast-boxes">
-                                <h4>${date}</h4>
-                                <img src="${iconUrl + castObj.weather[0].icon}.png" width="35px" height="35px" alt="weather-icon">
-                                <p>Temp: ${Math.round(castObj.main.temp)}°C</p>
-                                <p>Wind: ${castObj.wind.speed} KPH</p>
-                                <p>Humidity: ${castObj.main.humidity}%</p>
-                            </div>
-                            `);
-
-                            fiveDaysForecast = {
-                                Date: date,
-                                Temp: Math.round(castObj.main.temp),
-                                Wind: castObj.wind.speed,
-                                Humidity: castObj.main.humidity
-                            }
-
-                            forecastInfoArray.push(fiveDaysForecast);
-                        };
-                    };
-
-                    console.log(forecastInfoArray);
-                    saveTasks(forecastInfoArray);
-
-                    if(forecastData.cod == '200'){
-                        $('.historyCityName').removeClass("hide");
-                        $('.historyCityName').append(`
-                            <button class="btn">${cityName}</button>
-                        `);
-                    }
-
-                });
-        });
-};
+            cityNamesArray.push(cityName);
+            saveTasks(cityNamesArray);
+          }
+        }
+      });
+    });
+  }
+}
 
 // Local Storage Functions
 function saveTasks(arr) {
-    localStorage.setItem("forecast", JSON.stringify(arr));
-};
+  localStorage.setItem("forecast", JSON.stringify(arr));
+}
 
 function displayTasks() {
-    var forecastInfoArray = JSON.parse(localStorage.getItem("forecast")) || [];
-};
+  var cityNamesArray = JSON.parse(localStorage.getItem("forecast")) || [];
 
+  cityNamesArray.forEach(function(city) {
+    inputSubmitted(city);
+  });
+  
+}
 
-$('.searchBtn').click(function() {
-    var inputValue = $('#searchInput').val().split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');;
+displayTasks();
 
-    if(!inputValue){
-        alert("Please enter a city name!");
-    }
+// This function gets the city name from the input and runs the forecast function.
+$(".searchBtn").click(function () {
+  var inputValue = $("#searchInput")
+    .val()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(" ");
 
-    inputSubmitted(inputValue);
-
-    $('#searchInput').val('');
+  if (!inputValue) {
+    alert("Please enter a city name!");
+  }
+  inputSubmitted(inputValue);
+  $("#searchInput").val("");
 });
+
+// Bu calismiyor!
+$('.cityNameButtons').click(function() {
+    console.log("click!");
+});
+
 
 
 // console.log(`
@@ -107,4 +108,3 @@ $('.searchBtn').click(function() {
 // Wind: ${currentData.wind.speed},
 // IconUrl: ${iconUrl + currentData.weather[0].icon}.png
 // `);
-
